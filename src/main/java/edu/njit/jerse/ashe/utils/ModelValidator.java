9bb13b59380b3ef42465528774aa54b91e5be829
Jonathan Phillips <jonathan.m.phillips2@gmail.com>
2023-12-23 10:27:44 -0500

    adjusted JavaDocs for ModelValidator

--- src/main/java/edu/njit/jerse/ashe/utils/ModelValidator.java
+++ src/main/java/edu/njit/jerse/ashe/utils/ModelValidator.java
@@ -1,5 +1,6 @@
 package edu.njit.jerse.ashe.utils;
 
+import edu.njit.jerse.ashe.llm.openai.models.GptModel;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -24,7 +25,7 @@ public class ModelValidator {
      *
      * <p>Example models include:</p>
      * <ul>
-     *     <li>"gpt-4" - represents the GPT-4 model</li>
+     *     <li>"gpt-4" - represents the {@link GptModel#GPT_4} model</li>
      *     <li>"mock" - represents a mock model for testing or development purposes</li>
      * </ul>
      *
@@ -44,10 +45,10 @@ public class ModelValidator {
 
     /**
      * Validates the provided model name against a pre-defined set of valid models.
-     * If the model name is not in the list of valid models, an IllegalArgumentException is thrown.
+     * If the model name is not in the list of valid models, an {@link IllegalArgumentException} is thrown.
      *
      * @param model the model name to validate
-     * @throws IllegalArgumentException ff the provided model name is not in the list of valid models
+     * @throws IllegalArgumentException if the provided model name is not in the list of valid models
      */
     public static void validateModel(String model) {
         if (!VALID_MODELS.contains(model)) {
