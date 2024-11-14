a0e22c463b61e137b80b1fa56bb674fa1bc8987e
gmerr3 <127778000+gmerr3@users.noreply.github.com>
2024-11-14 15:30:14 -0500

    fix crash on methods in enums

--- src/main/java/edu/njit/jerse/ashe/utils/JavaCodeCorrector.java
+++ src/main/java/edu/njit/jerse/ashe/utils/JavaCodeCorrector.java
@@ -4,7 +4,6 @@ import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.NodeList;
-import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
 import com.github.javaparser.ast.body.TypeDeclaration;
 import com.github.javaparser.ast.expr.FieldAccessExpr;
@@ -138,7 +137,7 @@ public class JavaCodeCorrector {
     while (maxRetries > 0 && !errorOutput.isEmpty()) {
       String methodName = JavaCodeParser.extractMethodName(targetMethod);
       // TODO: this is wrong(should use fully qualified)
-      ClassOrInterfaceDeclaration checkedClass =
+      TypeDeclaration<?> checkedClass =
           JavaCodeParser.extractClassByMethodName(targetFile, methodName);
       String modelCorrection =
           fetchCorrectionFromModel(checkedClass.toString(), errorOutput, model);
